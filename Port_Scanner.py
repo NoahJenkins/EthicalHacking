0dec42dd2c09dbab1664608c340c2918186d98eb
Noah Jenkins <41129202+NoahJenkins@users.noreply.github.com>
2024-03-25 20:30:50 -0500

    port scanner

--- Port_Scanner.py
+++ Port_Scanner.py
@@ -1,4 +1,26 @@
 import socket
 import termcolor
 
-sock = socket.socket
+
+def scan(target, ports):
+    for port in range(1, ports):
+        scan_port(target, port)
+
+def scan_port(ipaddress, port):
+    try:
+        sock = socket.socket()
+        sock.connect((ipaddress, port))
+        print(f"[+] Port Opened " + str(port))
+    except:
+        print(f"[-] Port Closed " + str(port))
+
+
+targets = input("[*] Enter Targets IP Address (split them by comma): ")
+ports = int(input("[*] Enter how many ports you want to scan: "))
+if "," in targets:
+    print(termcolor.colored(("[*] Scanning Multiple Targets"), 'green'))
+    for ip_addr in targets.split(","):
+        scan(ip_addr.strip(" "), ports)
+
+           
+        
